e494b42728abd3b2ee560a342f9425380681e031
dotank <dotank300@gmail.com>
2022-06-28 09:41:29 +0300

    this is lab 3

---
+++ Lesson1/LAB3.py
@@ -0,0 +1,8 @@
+name="dotan"
+mail="dotank300@gmail.com"
+age=29
+
+print("name: " + name + "\nage: " + str(age) + "\nmail: " + mail)
+print("\n\nname: " + name[::-1] + "\nage: " + str(age*3) )
+print("dotan" in "ben dotan yuval roni gal or idan")
+
