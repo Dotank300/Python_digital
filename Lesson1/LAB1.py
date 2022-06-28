ed5ba2861af2fe0ffdb13c5fbd97485a19ed896a
dotank <dotank300@gmail.com>
2022-06-28 09:31:18 +0300

    this is lab 1

---
+++ Lesson1/LAB1.py
@@ -0,0 +1,21 @@
+num=int(input("Ente a number with 4 digit: "))
+'''
+Alafim=4
+Meot=5
+Asarot=6
+Ahadot=7
+'''
+
+#this is Alafim
+#print("Alafim " + str(num//1000))
+
+#this is Meot
+#print("Meot " + str((num%1000)//100))
+
+#this is Asarot
+#print("Asarot " + str((num%100)//10))
+
+#this is Ahadot
+#print("Ahadot " + str(num%10))
+
+print("Alafim" +str(num//1000) + "\nMeot=" + str((num%1000)//100) + "\nAsarot=" + str (((num%100)//10)) + "\nAhadot=" +str((num%10)) )
